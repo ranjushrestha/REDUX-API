e6a1fa5a60a6329bb9f650d7b3fda1d136fb8b53
Ranju Shrestha <ranjustha8844@gmail.com>
2026-05-01 15:16:50 +0545

    feat(collection): add clear collection functionality and improve UI logic

--- src/pages/CollectionPage.tsx
+++ src/pages/CollectionPage.tsx
@@ -1,19 +1,22 @@
 import { Link } from "react-router-dom";
 import SearchBar from "../components/SearchBar";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import type { RootState } from "../redux/store";
 import CollectionCard from "../components/CollectionCard";
+import { clearCollection } from "../redux/features/collectionSlice";
 
 const CollectionPage = () => {
   const collection = useSelector(
     (state: RootState) => state.collection.items
   );
 
+  const dispatch = useDispatch()
+
   return (
     <div className="min-h-screen px-6 py-6">
 
       {/* Navbar */}
-      <div className="flex justify-between items-center mb-6">
+      <div className="flex gap-6 justify-between items-center mb-6">
         <Link to="/" className="font-medium">
           Search
         </Link>
@@ -23,9 +26,18 @@ const CollectionPage = () => {
       </div>
 
       {/* Search */}
-      <div className="mb-6">
-        <SearchBar />
+    
+      <div>
+        {collection.length > 0 && (
+          <button
+       onClick={() => dispatch(clearCollection())}
+       
+       className="bg-red-500 px-5 py-2 rounded">Clear Collection</button>
+        )}
+       
       </div>
+
+
 
       {/* Grid */}
       <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-6">
